410e2244b9e4c54ed685cd7bd3bef4cbfe6b429e
Irving78rus <54670767+Irving78rus@users.noreply.github.com>
2020-08-29 00:15:01 +0300

    Update main2.js

--- main2.js
+++ main2.js
@@ -17,10 +17,10 @@ var containerProductInBasket = document.createElement("div");
     allBasket.appendChild(containerProductInBasket);
     containerProductInBasket.classList.add("containerProductInBasket");
 
-var headerBasket = document.createElement("div");
+    var headerBasket = document.createElement("div");
     allBasket.appendChild(headerBasket);
-    headerBasket.classList.add("headerBasket");
-    headerBasket.append("Заглушка: общая стоимость товара  = allCost");    
+    headerBasket.classList.add("costBasket");
+    headerBasket.append("В корзине нет товаров");     
 
 
 
@@ -38,8 +38,13 @@ var basket = [
     
 ];
 
+
+// var a = 0;
 // считывает нажатие по ай-ди и добавляет товар в наш баскет
-function buttonclick(e){
+
+
+
+function buttonclickPlus(e){
     var targid = e.target.id
     var foundProduct = clothes.find(func => func.id == targid)
      
@@ -50,15 +55,48 @@ function buttonclick(e){
     else if(basket.indexOf(foundProduct) > -1){
         foundProduct.count++
     }
-    var count = foundProduct.count
-    console.log(count)
+        
     console.log(basket)
-    
-    document.querySelector('.containerProductInBasket').innerHTML = '';
-    rednerBasket(count);
-    return count
-}
 
+//     for (var i = 0; i < basket.length; i++) {
+
+//   a = basket[1].price * basket[1].count
+//   console.log(a)
+//     }
+
+
+    document.querySelector('.costBasket').remove();
+     
+    var headerBasket = document.createElement("div");
+    allBasket.appendChild(headerBasket);
+    headerBasket.classList.add("costBasket");
+    headerBasket.append("Общая стоимость покупок " + "allSum"); 
+    document.querySelector('.containerProductInBasket').innerHTML = '';
+    rednerBasket();
+    
+}
+function buttonclickMinus(e){
+    var targid = e.target.id
+    var foundProduct = clothes.find(func => func.id == targid)
+     
+    if(basket.indexOf(foundProduct) == -1){
+      foundProduct.count = 1
+      basket.push(foundProduct) 
+    }
+    else if(basket.indexOf(foundProduct) > -1){
+        foundProduct.count--
+    }
+    console.log(basket)
+    document.querySelector('.costBasket').remove();
+     
+    var headerBasket = document.createElement("div");
+    allBasket.appendChild(headerBasket);
+    headerBasket.classList.add("costBasket");
+    headerBasket.append("Общая стоимость покупок " + 'allSum'); 
+    document.querySelector('.containerProductInBasket').innerHTML = '';
+    rednerBasket();
+    
+}
 
 
 // Отрисовываем каталог
@@ -86,29 +124,32 @@ function createCard(any){
     button.innerHTML = "Добавить товар в корзину";
     button.className = "buttonIn";
     button.id = any.id;
-    button.onclick = buttonclick
+    button.onclick = buttonclickPlus
     return div
 }
+
 
 
 //rednerBasket Отрисовывает товары в корзине
 function rednerBasket() {
     for (var i = 0; i < basket.length; i++) {
-        var cardElement = createCardInBasket(basket[i])
-        containerProductInBasket.append(cardElement); // вызвал
         
+        var cardElement = createCardInBasket(basket[i]);
+        containerProductInBasket.append(cardElement); // вызвал
+
     }
 }
 rednerBasket();
 
 
 //createCardInBasket создает товар
-function createCardInBasket(any,count){
+function createCardInBasket(any){
     
     var div = document.createElement('div');
     div.className = "clothesInBasket";
     div.append(any.name + "\n");
     div.append("Цена : " + any.price + 'р');
+    div.id = any.id + "block"
 
     var img = document.createElement("IMG");
     img.src = any.img;
@@ -119,33 +160,27 @@ function createCardInBasket(any,count){
     div.append(buttonPlus);
     buttonPlus.innerHTML = "Добавить единицу товара";
     buttonPlus.className = "buttonBasket";
-    buttonPlus.id = any.id + "Plus";
+    buttonPlus.id = any.id ;
+    buttonPlus.onclick = buttonclickPlus
 
     var buttonMinus = document.createElement("button");
     div.append(buttonMinus);
     buttonMinus.innerHTML = "Убрать единицу товара";
     buttonMinus.className = "buttonBasket";
-    buttonMinus.id = any.id + "Minus";
-
+    buttonMinus.id = any.id ;
+    buttonMinus.onclick = buttonclickMinus
 
     var counter = document.createElement('div');
     counter.className = "headerBasket";
+
+    if(any.count<0){
+        any.count =0
+    }
+   
     div.append("Общая Цена: " + any.price * any.count +' р');
     
-
-
-
-
-    
-
     return div
 }
-
-
-
-
-   
-
 
 
 
