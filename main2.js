d51c98d263ca483bb89f47357e167936c3cf38ec
Irving78rus <54670767+Irving78rus@users.noreply.github.com>
2020-08-28 16:08:46 +0300

    Update main2.js

--- main2.js
+++ main2.js
@@ -1,52 +1,111 @@
 //Статика
 var product = document.createElement("div");
-document.body.appendChild(product);
-product.classList.add("product");
+    document.body.appendChild(product);
+    product.classList.add("product");
 
 var allBasket = document.createElement("div");
     document.body.appendChild(allBasket);
     allBasket.classList.add("basket");
     allBasket.id = 'allBasket';
-    allBasket.append("Корзина товаров");
+
+var headerBasket = document.createElement("div");
+    allBasket.appendChild(headerBasket);
+    headerBasket.classList.add("headerBasket");
+    headerBasket.append("Корзина товаров");
+    
+var containerProductInBasket = document.createElement("div");
+    allBasket.appendChild(containerProductInBasket);
+    containerProductInBasket.classList.add("containerProductInBasket");
+
+
 
 
 // Массив с объектами товаров    
 var clothes = [
-    { id: 'q1234', name: 'maika', price: 1000, img: 'img/shirt.png' },
-    { id: 'w1234', name: 'boots', price: 2000, img: 'img/boots.png' },
-    { id: 'e1234', name: 'legs', price: 3000, img: 'img/jeans.png' },
+    { id: 'q1234', name: 'Shirt', price: 1000, img: 'img/shirt.png' },
+    { id: 'w1234', name: 'Boots', price: 2000, img: 'img/boots.png' },
+    { id: 'e1234', name: 'Jeans', price: 3000, img: 'img/jeans.png' },
 ];
 
-
+// Basket это массив , в который мы будем помещать объект, на который кликнули в каталоге
+// Пока он просто заполнен для наглядности
+var basket = [
+    { id: 'q1234', name: 'Shirt', price: 1000, img: 'img/shirt.png' },
+    { id: 'w1234', name: 'Boots', price: 2000, img: 'img/boots.png' },
+    { id: 'e1234', name: 'Jeans', price: 3000, img: 'img/jeans.png' },
+];
 
 // Отрисовываем каталог
 function rednerCatalog() {
     for (var i = 0; i < clothes.length; i++) {
-        var div = document.createElement('div');
-        product.append(div);
-        div.className = "block";
-        div.append(clothes[i].name );
-        div.append("Цена : " + clothes[i].price + 'р');
-
-
-        var img = document.createElement("IMG");
-        img.src = clothes[i].img;
-        div.appendChild(img);
-        img.className = "img";
-
-        var button = document.createElement("button");
-        div.append(button);
-        button.innerHTML = "Добавить товар в корзину";
-        button.className = "buttonIn";
-        button.id = clothes[i].id;
+        var cardElement = createCard(clothes[i])
+        product.append(cardElement); // вызвал
     }
 }
 rednerCatalog();
 
+function createCard(any){
+    var div = document.createElement('div');
+    div.className = "block";
+    div.append(any.name + "\n");
+    div.append("Price : " + any.price + 'р');
+
+    var img = document.createElement("IMG");
+    img.src = any.img;
+    div.appendChild(img);
+    img.className = "img";
+
+    var button = document.createElement("button");
+    div.append(button);
+    button.innerHTML = "Добавить товар в корзину";
+    button.className = "buttonIn";
+    button.id = any.id;
+    return div
+}
+
+
+//rednerBasket Отрисовывает товары в корзине
+function rednerBasket() {
+    for (var i = 0; i < basket.length; i++) {
+        var cardElement = createCardInBasket(basket[i])
+        containerProductInBasket.append(cardElement); // вызвал
+    }
+}
+rednerBasket();
+
+
+//createCardInBasket создает товар
+function createCardInBasket(any){
+    var div = document.createElement('div');
+    div.className = "clothesInBasket";
+    div.append(any.name + "\n");
+    div.append("Цена : " + any.price + 'р');
+
+    var img = document.createElement("IMG");
+    img.src = any.img;
+    div.appendChild(img);
+    img.className = "basketImg";
+
+    var buttonPlus = document.createElement("button");
+    div.append(buttonPlus);
+    buttonPlus.innerHTML = "Добавить товар в корзину";
+    buttonPlus.className = "buttonBasket";
+    buttonPlus.id = any.id + "Plus";
+
+    var buttonMinus = document.createElement("button");
+    div.append(buttonMinus);
+    buttonMinus.innerHTML = "Убрать товар из корзины";
+    buttonMinus.className = "buttonBasket";
+    buttonMinus.id = any.id + "Minus";
+    div.append('заглушка = basket.prise*counter');
+
+    return div
+}
 
 
 
 
+   
 
 
 
